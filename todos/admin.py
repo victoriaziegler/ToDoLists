6343c9804321b69f1153fea03cb51dc44940fb6a
Victoria Ziegler <toriziegler@cox.net>
2022-06-14 11:54:20 -0700

    Feature 6 Complete

--- todos/admin.py
+++ todos/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from todos.models import TodoList
+from todos.models import TodoList, TodoItem
 
 # Register your models here.
 
@@ -8,4 +8,9 @@ class TodoListAdmin(admin.ModelAdmin):
     pass
 
 
+class TodoItemAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(TodoList, TodoListAdmin)
+admin.site.register(TodoItem, TodoItemAdmin)
